8135b4556eb2312e92b1544040a19aa6ada1781a
ChoiWonYu <phillip07290@naver.com>
2023-07-04 01:02:51 +0900

    feat: create user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  userList(@Body() userEmail: Prisma.UserWhereUniqueInput) {
+    return this.userService.findUser(userEmail);
+  }
+
+  @Post('/signup')
+  signup(@Body() loginInfo: Prisma.UserCreateInput) {
+    return this.userService.createUser(loginInfo);
+  }
+}
